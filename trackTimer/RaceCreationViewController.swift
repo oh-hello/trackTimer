ea0d81b17b5e4b16c37194a1458bc597ebe29828
MADELINE SANDISH <758278@ehs-s-232-33.local>
2017-12-21 12:31:04 -0600

    Add navigation to race creation view controller Attempt to set dimensions for picker view

--- trackTimer/RaceCreationViewController.swift
+++ trackTimer/RaceCreationViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import os.log
 
-class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+class RaceCreationViewController: UIViewController,UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
     let pickerData = [[1,2,3,4,5,6,7,8,9,10]]
     
     //MARK: Properties
-    @IBOutlet weak var sectionTitle: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var locationField: UITextField!
     @IBOutlet weak var distanceField: UITextField!
     @IBOutlet weak var numberPicker: UIPickerView!
     @IBOutlet weak var pickerTest: UILabel! //delete this outlet later
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    
+    //Race contstructed based on race info input by user
+    var race: Race?
+
     
     //MARK: Life Cycle
     
@@ -26,7 +32,14 @@ class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPick
         super.viewDidLoad()
         numberPicker.delegate = self
         numberPicker.dataSource = self
+        // Handle the text field’s user input through delegate callbacks.
+        locationField.delegate = self
+        distanceField.delegate = self
 
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(false)
         //automatically sets race date when view loads
         let currentDate = NSDate()
         let dateFormatter = DateFormatter()
@@ -39,9 +52,6 @@ class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPick
         numberPicker.selectRow(0, inComponent: 0, animated: false)
         updateLabel()
         
-        //test values in UITextFields
-        
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -49,7 +59,7 @@ class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPick
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: Delegate and Data Source
+    //MARK: UIPickerView Delegate and Data Source
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return pickerData.count
     }
@@ -66,7 +76,18 @@ class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPick
         return String(pickerData[component][row])
     }
     
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //Hide the Keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
     //MARK: Actions
+    
+    //MARK: Private Methods
     
     //updates the label based on picker view selection
     
@@ -82,16 +103,38 @@ class RaceCreationViewController: UIViewController,UIPickerViewDataSource,UIPick
         pickerTest.text = String(numRunners)
     }
     
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return 200
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
     
 
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: false, completion: nil)
     }
-    */
+    
+    //configure view controller before it's presented
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for:segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let date = dateLabel.text
+        let location = locationField.text ?? "Practice"
+        let distance = distanceField.text ?? ""
+        let runnerList = [Runner(nameFirst: "Test", nameLast: "test")]
+        
+        race = Race(date: date!, location: location, distance: distance, runnerList: runnerList as! Array<Runner>)
+    }
 
 }
