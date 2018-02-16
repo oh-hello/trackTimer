e49b4cee392720e6669c8cef9e7eb35944642cc5
KRUEGER, LILLIAN <755852@ehs-s-232-45.local>
2018-02-16 11:40:42 -0600

    The table is now sorted in order from newest to oldest

--- trackTimer/ArchiveViewController.swift
+++ trackTimer/ArchiveViewController.swift
@@ -12,18 +12,25 @@ var myIndex = 0
 
 class ArchiveViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var sortedRaces = [Race]()
     @IBOutlet weak var archiveTable: UITableView!
     
+    func sortRaces(){
+        sortedRaces = allRaces.reversed()
+    }
+    
+    //MARK: Private Functions
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allRaces.count
+        return sortedRaces.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = allRaces[indexPath.row].date
+        cell.textLabel?.text = sortedRaces[indexPath.row].date + " " + sortedRaces[indexPath.row].location + " " + sortedRaces[indexPath.row].distance!
         return cell
     }
     
+    //MARK: Navigation
     internal func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         myIndex = indexPath.row
         performSegue(withIdentifier: "toRaceReportFromArchive", sender: self)
@@ -37,7 +44,7 @@ class ArchiveViewController: UIViewController, UITableViewDelegate, UITableViewD
             back(sender as Any)
         default:
             let destination = segue.destination as? RaceReportViewController
-            destination!.race = allRaces[myIndex]
+            destination!.race = sortedRaces[myIndex]
         }
     }
     
@@ -45,9 +52,9 @@ class ArchiveViewController: UIViewController, UITableViewDelegate, UITableViewD
         dismiss(animated: false, completion: nil)
     }
     
-    
     override func viewDidLoad(){
         super.viewDidLoad()
+        sortRaces()
     }
  
     
