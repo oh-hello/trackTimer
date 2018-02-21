b015e996b22a398f1155c89f6a1eeb25feb8dccf
MADELINE SANDISH <758278@ehs-s-232-33.local>
2018-02-21 12:24:55 -0600

    Add relevant comments to archive

--- trackTimer/ArchiveViewController.swift
+++ trackTimer/ArchiveViewController.swift
@@ -28,6 +28,7 @@ class ArchiveViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            //delete race from array and table
             allRaces.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
             
